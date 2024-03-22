07d0939e47532a5402371f3ba899e93b5199cd25
Anne Hunt <161772537+Anne-Hunt@users.noreply.github.com>
2024-03-22 15:12:02 -0600

    html mostly done, javascript halfway

--- app.js
+++ app.js
@@ -1,1 +1,102 @@
-let cult = []
+let upgrades = [
+    {
+        name: 'bullhorn',
+        power: 5,
+        timer: 0,
+        count: 0,
+        cost: 10
+    },
+    {
+        name: 'tv show',
+        power: 15,
+        timer: 0,
+        count: 0,
+        cost: 100
+    },
+    {
+        name: 'cult leaders',
+        power: 20,
+        timer: 3000,
+        count: 0,
+        cost: 500
+    },
+    {
+        name: 'mlm',
+        power: 100,
+        timer: 300000,
+        count: 0,
+        cost: 1000
+    }
+]
+
+let totalFollowing = 0
+let followers = 0
+let clock = upgrades[2].timer
+
+drawStats()
+
+function mineFollowers() {
+    followers++
+    drawStats()
+}
+
+function addOutreach(upgradeName) {
+    let improvement = upgrades.find(upgrade => upgrade.name == upgradeName)
+    improvement.cost
+    improvement.count
+    improvement.value
+}
+
+function addCultLeader() {
+
+}
+
+setInterval(addCultLeader, upgrades[2].timer)
+
+setInterval(moveFollowers, 5000)
+
+function moveFollowers() {
+    totalFollowing += followers
+    followers = 0
+    drawStats()
+}
+
+function drawStats() {
+    let bullhornElem = document.getElementById('bullhorn');
+    let tvshowElem = document.getElementById('tvshow');
+    let cultLeaderElem = document.getElementById('cultLeader');
+    let mlmElem = document.getElementById('mlm');
+    let clockElem = document.getElementById('clockCount');
+    let followingElem = document.getElementById('totalFollowingCount');
+    let followerElem = document.getElementById('followersCount');
+    let bullhornBtnElem = document.getElementById('bullhornbtn');
+    let tvshowBtnElem = document.getElementById('tvshowbtn');
+    let cultleaderBtnElem = document.getElementById('cultleaderbtn');
+    let mlmbtnElem = document.getElementById('mlmbtn');
+
+    followerElem.innerHTML = `<span class="mdi mdi-account-multiple-plus"></span><span> ${followers}</span>`
+    followingElem.innerHTML = `<span class="mdi mdi-account-group"></span><span> ${totalFollowing}</span>`
+    clockElem.innerHTML = `<span class="mdi mdi-clock-plus-outline"></span><span> ${clock}</span>`
+    bullhornBtnElem.innerHTML = `<span
+    class="mdi mdi-bullhorn-variant"></span><span> ${upgrades[0].cost}</span>`
+    tvshowBtnElem.innerHTML = `<span
+    class="mdi mdi-television-classic"></span><span> ${upgrades[1].cost}</span>`
+    cultleaderBtnElem.innerHTML = `<span
+    class="mdi mdi-human-handsup"></span><span> ${upgrades[2].cost}</span>`
+    mlmbtnElem.innerHTML = `<span class="mdi mdi-pyramid"></span><span> ${upgrades[3].cost}</span>`
+    mlmElem.innerHTML = `<span class="mdi mdi-pyramid"><span> ${upgrades[3].count}</span>`
+    cultLeaderElem.innerHTML = `<span
+    class="mdi mdi-human-handsup"><span> ${upgrades[2].count}</span>`
+    tvshowElem.innerHTML = `<span
+    class="mdi mdi-television-classic"><span> ${upgrades[1].count}</span>`
+    bullhornElem.innerHTML = `<span
+    class="mdi mdi-bullhorn-variant"><span> ${upgrades[0].count}</span>`
+}
+
+function reset() {
+    totalFollowing = 0
+    followers = 0
+    for (let i = 0; i < upgrades.length; i++) {
+        upgrades[i].count = 0
+    }
+}
